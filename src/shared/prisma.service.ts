46e1249589bb6ac37a23aaf267cef50e6594aae6
Sandy-Thabet <sandythabit588@gmail.com>
2024-07-26 20:53:47 +0300

    Movie

--- src/shared/prisma.service.ts
+++ src/shared/prisma.service.ts
@@ -1,5 +1,5 @@
 import { OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from 'prisma/generated/client';
 
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
